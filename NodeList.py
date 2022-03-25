b1ead82d5b153dee033c6f9cff4c8a93e586de59
Daniel Lin <toramame91@gmail.com>
2022-03-24 18:10:26 -0700

    -added in Node list for Ephemeral Nodes

--- NodeList.py
+++ NodeList.py
@@ -1,9 +1,16 @@
 class NodeLists:
     def __init__(self):
         self._ewNodeListLegendary = [34043, 34044, 34045, 33967, 33968, 33969]
+        self._ewNodeListEphemeral = [34047, 34048, 34050, 34052]
 
     def GetEndwalkerListLegendary(self):
         return self._ewNodeListLegendary
 
     def EndwalkerListLegendaryCount(self):
         return len(self._ewNodeListLegendary)
+
+    def GetEndwalkerListEphemeral(self):
+        return self._ewNodeListEphemeral
+
+    def EndwalkerListEphemeralCount(self):
+        return len(self._ewNodeListEphemeral)
